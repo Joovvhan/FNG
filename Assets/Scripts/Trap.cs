2f635e0d8e3d19f6a078c1cb059c22f393b43290
Joowhan Song <joovvhan.sonq@gmail.com>
2021-12-13 02:45:49 +0900

    Trap

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField] GameObject needle;
+    [SerializeField] private int needleCycle;
+    protected GameManager gameManager;
+    protected Player player;
+    private int turn = 0;
+
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.AddTrapToList(this);
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    public IEnumerator SetNeedle()
+    {
+        turn += 1;
+        if (turn % needleCycle == 0)
+        {
+            needle.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+            if (gameManager.GetPlayerPosition() == (int)transform.position.x)
+            {
+                yield return StartCoroutine(player.LoseHP(1.0f));
+            }
+        }
+        else
+        {
+            needle.SetActive(false);
+        }
+    }
+
+}
